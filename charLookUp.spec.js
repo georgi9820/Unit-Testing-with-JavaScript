47566c65687cd9e595c3a2ec2960b1e8d01edfcf
Georgi Georgiev <63241083+georgi9820@users.noreply.github.com>
2024-01-26 21:26:44 +0200

    New 4 test cases in charLookUp.spec.js file

--- charLookUp.spec.js
+++ charLookUp.spec.js
@@ -30,5 +30,39 @@ describe("lookupChar", () => {
         expect(result).to.be.undefined;
     })
 
+    it("should return index is incorrect when the index is bigger than the string length", () => {
+        const correctFirstParam = "hello";
+        const correctSecondParam = 7;
+        
+        const result = lookupChar(correctFirstParam, correctSecondParam);
 
+        expect(result).to.be.equal("Incorrect index");
+    })
+
+    it("should return index is incorrect when the index is equal to the string length", () => {
+        const correctFirstParam = "hello";
+        const correctSecondParam = 5;
+        
+        const result = lookupChar(correctFirstParam, correctSecondParam);
+
+        expect(result).to.be.equal("Incorrect index");
+    })
+
+    it("should return index is incorrect when the index is negative number", () => {
+        const correctFirstParam = "hello";
+        const correctSecondParam = -2;
+        
+        const result = lookupChar(correctFirstParam, correctSecondParam);
+
+        expect(result).to.be.equal("Incorrect index");
+    })
+
+    it("should return correct char relative to the index", () => {
+        const correctFirstParam = "hello";
+        const correctSecondParam = 1;
+        
+        const result = lookupChar(correctFirstParam, correctSecondParam);
+
+        expect(result).to.be.equal("e");
+    })
 })
